1dacf0ff5b2171d28884ebb66c8dd0a1ead4e0d6
chen li kuan <clikuan.cs05g@g2.nctu.edu.tw>
2017-09-16 14:44:49 +0800

    add icon

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ const {app, BrowserWindow} = require('electron');
 let win;
 
 function createWindow () {
-	win = new BrowserWindow({width: 800, height: 1500,
-        webPreferences: {
-            nodeIntegration: false }
-    });
+	win = new BrowserWindow(
+		{
+			width: 800,
+		   	height: 1500,
+			icon: __dirname + '/favicon.ico',
+        	webPreferences: {
+            	nodeIntegration: false
+		   }
+    	}
+	);
 	win.loadURL('http://140.113.199.199:7788/connection');
 	let contents = win.webContents;
 	console.log(contents);
